7e12646a66682c6d561f7b82bb69d3381029fef4
Austinb96 <austin.m.babcock@gmaIl.com>
2022-03-25 09:42:20 -0500

    Day of week working and other styling

--- src/assets/js/clock.js
+++ src/assets/js/clock.js
@@ -1,17 +1,21 @@
 function currentTime() {
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let date = new Date();
-    let dayOfWeek = date.getUTCDay();
+    let dayOfWeek = weekday[date.getDay()];
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
     let session = "AM";
     
-    if(hh==0){
+    var militaryTimeToggle = document.getElementById("militaryTimeToggle");
+    
+    
+    if(hh===0){
         hh = 12;
     }
     
-    if(hh > 12) {
-        hh == hh - 12;
+    if(hh > 12 && militaryTimeToggle[i].checked) {
+        hh = hh - 12;
         session = "PM";
     }
     
@@ -21,7 +25,8 @@ function currentTime() {
     
     let time = hh + ":" + mm + ":" + ss + " " + session;
     
-    document.getElementById("clock").innerText = time;
+    document.getElementById("clock").innerHTML = time;
+    document.getElementById("dayOfWeek").innerHTML = dayOfWeek;
     let t = setTimeout(function(){currentTime()}, 1000);
 }
 
